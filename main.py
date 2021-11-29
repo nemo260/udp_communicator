b5abbd231ade60b41ba40099197a70cc64fabf97
nemo260 <nneemmeecc.m@gmail.com>
2021-11-29 15:28:44 +0100

    1

--- main.py
+++ main.py
@@ -6,60 +6,54 @@ import time
 def recieving(sock):
     sock.bind((udp_ip, udp_port))
     while True:
-        data, addr = sock.recvfrom(1500)
+        data, addr = sock.recvfrom(1024)
         print("Recieved message: " + data)
 
 
-#
-#
-# """
-# t1 = threading.Thread(target=recieving(sock), name="t1")
-# t1.start()
-# """
-#
-# message = "Dobar dan"
-# message = bytes(message, "utf-8")
-# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#
-# sock.sendto(message, (udp_ip, udp_port))
-
-def send_message(sock):
+def send_message():
     message = input("Write your message: \n")
     message = bytes(message, "utf-8")
     sock.sendto(message, (udp_ip, udp_port))
     print("Message: [" + str(message) + "] was send")
 
+
 def client_mode():
-    print("Program is running as a Client...")
+    print("!!! Program is running as a Client !!!")
     time.sleep(1)
-    print("**********************************")
-    message_or_file = int(input("If you want to send message, press 0\nIf you want to send file, press 1\n"))
-    if message_or_file == 0:
-        send_message(sock)
+    while True:
+        print()
+        message_or_file = int(input(
+            "If you want to send message, press 0\nIf you want to send file, press 1\nIf you want to change mode, press 2\n"))
+        if message_or_file == 0:
+            send_message()
+        if message_or_file == 2:
+            starting()
+
 
 def server_mode():
-    print("Program is running as a Server...")
+    print("!!! Program is running as a Server !!!")
     time.sleep(1)
-    print("Program is waiting for communication...")
 
     recieving(sock)
+    print()
+    message_or_file = int(input("If you want to recieving, press 0\nIf you want to change mode, press 2\n"))
+    if message_or_file == 0:
+       recieving(sock)
+    if message_or_file == 2:
+       starting()
 
 
 def starting():
-    while True:
-        try:
-            option = int(input("Press 0 for Client mode or press 1 for Server mode\n"))
-            if option == 0:
-                client_mode()
-            if option == 1:
-                server_mode()
-        except:
-            print("Bad input")
+    option = int(input("Press 0 for Client mode or press 1 for Server mode\n"))
+    if option == 0:
+        client_mode()
+    if option == 1:
+        server_mode()
 
 
 udp_ip = "192.168.0.104"
 udp_port = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print("Program starting...")
-time.sleep(0.5)
+time.sleep(1)
 starting()
